160f0e79633ec44834273041f721ae24e1d5e3f2
groostav-desktop <geoff_groos@msn.com>
2016-04-03 15:49:53 -0700

    updated porter
    removed annoying default gitignore line,
    added what I think will be needed libs for porter

--- porter/src/ca/sfu/cs/porter/Porter.kt
+++ porter/src/ca/sfu/cs/porter/Porter.kt
@@ -1,9 +1,7 @@
 package ca.sfu.cs.porter
 
 import org.apache.commons.cli.CommandLine
-import org.apache.commons.cli.DefaultParser
 import org.apache.commons.cli.Options
-import org.apache.commons.cli.PosixParser
 
 /**
  * Created by Geoff on 4/3/2016.
