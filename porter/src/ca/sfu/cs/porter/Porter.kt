e71dfcb355d561fa5b80face595dbc1040e8710c
groostav-desktop <geoff_groos@msn.com>
2016-04-03 15:17:09 -0700

    added skeleton project

---
+++ porter/src/ca/sfu/cs/porter/Porter.kt
@@ -0,0 +1,31 @@
+package ca.sfu.cs.porter
+
+import org.apache.commons.cli.CommandLine
+import org.apache.commons.cli.DefaultParser
+import org.apache.commons.cli.Options
+import org.apache.commons.cli.PosixParser
+
+/**
+ * Created by Geoff on 4/3/2016.
+ */
+fun main(args: Array<String>){
+
+}
+
+class CommandLineInterpreter{
+
+    val options = Options();
+
+    constructor(){
+        options.addOption(
+                ""
+        )
+    }
+
+    fun parse(args : Array<String>) : CommandLine {
+
+    }
+
+}
+
+
